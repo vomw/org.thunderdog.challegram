a01a1d52ae03864b3a21de8b9f1b2831c2d6e6ac
vomw <22770640+vomw@users.noreply.github.com>
2026-01-28 18:45:02 +0800

    rm

--- app/src/main/java/org/thunderdog/challegram/telegram/ForumTopicInfoListener.java
+++
@@ -1,8 +0,0 @@
-package org.thunderdog.challegram.telegram;
-
-import org.drinkless.tdlib.TdApi;
-
-public interface ForumTopicInfoListener {
-  default void onForumTopicInfoChanged (TdApi.ForumTopicInfo info) { }
-  default void onForumTopicUpdated (long chatId, long messageThreadId, boolean isPinned, long lastReadInboxMessageId, long lastReadOutboxMessageId, TdApi.ChatNotificationSettings notificationSettings) { }
-}
--- extension/hms/src/main/kotlin/tgx/extension/hms/Huawei.kt
+++
@@ -1,7 +0,0 @@
-package tgx.extension.hms
-
-import android.content.Context
-import com.huawei.agconnect.AGConnectOptionsBuilder
-
-fun obtainHuaweiAppId(context: Context): String =
-  AGConnectOptionsBuilder().build(context).getString("/client/app_id") ?: ""
